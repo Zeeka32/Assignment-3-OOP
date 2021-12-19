d9c237e5afae9424efcdab009bf8fb088a56265f
Achilles404 <spyvip123@gmail.com>
2021-12-19 18:37:17 +0200

    Some changes

--- S12_20200161_20201127.cpp
+++ S12_20200161_20201127.cpp
@@ -3,9 +3,71 @@
 
 using namespace std;
 
+//TODO
+class FloatArray {
+private:
+    int size_;
+    float *arr;
+public:
+    FloatArray(int size) {
+        size_ = size;
+        arr = new float[size_];
+        for (int i = 0; i < size_; ++i) {
+            arr[i] = 0;
+        }
+    }
+
+    void addFloat(float x) {
+        for (int i = 0; i < size_; ++i) {
+            if (arr[i] == 0) {
+                arr[i] = x;
+                break;
+            }
+        }
+    }
+
+
+    friend void operator<<(ofstream &in, FloatArray &rhs) {
+
+    }
+
+    friend void operator>>(ofstream &in, FloatArray &rhs) {
+
+    }
+
+    ~FloatArray() {
+        delete[] arr;
+    }
+
+//    void print() {
+//        for (int i = 0; i < size_; ++i) {
+//            cout << arr[i] << " ";
+//        }
+//    }
+};
+
+//TODO
+class SortedArray {
+
+};
+
+//TODO
+class FrontArray {
+
+};
+
+//TODO
+class PositiveArray {
+
+};
+
+//TODO
+class NegativeArray {
+
+};
 
 int main() {
-    cout << "hahahaahaha\n";
-    cout << "Hello world";
-    cout << "hahaha\n";
+    FloatArray x(3);
+    x.addFloat(1.2);
+//    x.print();
 }
