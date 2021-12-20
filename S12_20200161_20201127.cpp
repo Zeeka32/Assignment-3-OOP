b057615805fc98bff6140ed89a91b5afb6b47c60
Zeeka32 <Zeeka32@gmail.com>
2021-12-20 07:27:19 +0200

    Update S12_20200161_20201127.cpp
    added prototypes for all constructors and member functions.

--- S12_20200161_20201127.cpp
+++ S12_20200161_20201127.cpp
@@ -1,42 +1,47 @@
 //Amr Riyad - 20201127 || Hussien Mostafa - 20200161
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 //TODO
 class FloatArray {
-private:
+protected:
+    float *arr_;
     int size_;
-    float *arr;
 public:
     FloatArray(int size) {
         size_ = size;
-        arr = new float[size_];
+        arr_ = new float[size_];
         for (int i = 0; i < size_; ++i) {
-            arr[i] = 0;
+            arr_[i] = 0;
         }
     }
 
-    void addFloat(float x) {
+    void addFloatToBack(float x) {
         for (int i = 0; i < size_; ++i) {
-            if (arr[i] == 0) {
-                arr[i] = x;
+            if (arr_[i] == 0) {
+                arr_[i] = x;
                 break;
             }
         }
     }
 
-
-    friend void operator<<(ofstream &in, FloatArray &rhs) {
-
+    //YASTA A7MOS ANA M4 FAHM 7AGA FI EL FILES
+    friend ostream &operator<<(ostream &output, const FloatArray &rhs) {
+        ofstream outFile("out.txt");
+            for(int i = 0; i < rhs.size_; i++){
+                outFile << rhs.arr_[i] << " ";
+            }
+            return output;
     }
 
-    friend void operator>>(ofstream &in, FloatArray &rhs) {
+    friend istream &operator>>(istream &input, FloatArray &rhs) {
 
     }
 
     ~FloatArray() {
-        delete[] arr;
+        delete[] arr_;
     }
 
 //    void print() {
@@ -47,27 +52,47 @@ public:
 };
 
 //TODO
-class SortedArray {
+class SortedArray : public FloatArray {
+public:
+    SortedArray(int size) : FloatArray(size){}
+
+    void addSortedFloat(float f){
+        
+    }
 
 };
 
 //TODO
-class FrontArray {
+class FrontArray : public FloatArray {
+public:
+    FrontArray(int size) : FloatArray(size){}
 
+    void addFloatToFront(float f){
+        
+    }
 };
 
 //TODO
-class PositiveArray {
+class PositiveArray : public SortedArray {
+    PositiveArray(int size) : SortedArray(size){}
 
+    void addPostiveFloat(float f){
+
+    }
 };
 
 //TODO
-class NegativeArray {
+class NegativeArray : public SortedArray {
+    NegativeArray(int size) : SortedArray(size){}
 
+    void addNegativeFloat(float f){
+
+    }
 };
 
 int main() {
     FloatArray x(3);
-    x.addFloat(1.2);
+    x.addFloatToBack(1.2);
 //    x.print();
+
 }
